5cf96df5f12cb9f9d7b0dffc2d4e8fa1abade129
chronosgit <warfacemeister@mail.ru>
2025-04-11 20:03:32 +0500

    🗑️ remove: removed sidebar related stuff from global layout ctx

--- src/contexts/GlobalLayoutContext.tsx
+++ src/contexts/GlobalLayoutContext.tsx
@@ -1,11 +1,8 @@
-import useToggle from '@/hooks/useToggle';
 import { createContext, ReactNode } from 'react';
+import useToggle from '@/hooks/useToggle';
 
 interface GlobalLayoutContext {
-	isSidebar: boolean;
 	isMobileSidebar: boolean;
-	changeSidebar: (newValue: boolean) => unknown;
-	toggleSidebar: Function;
 	changeMobileSidebar: (newValue: boolean) => unknown;
 	toggleMobileSidebar: Function;
 }
@@ -22,12 +19,6 @@ const GlobalLayoutContextProvider = ({
 	children,
 }: GlobalLayoutContextProviderProps) => {
 	const {
-		value: isSidebar,
-		changeValue: changeSidebar,
-		toggleValue: toggleSidebar,
-	} = useToggle();
-
-	const {
 		value: isMobileSidebar,
 		changeValue: changeMobileSidebar,
 		toggleValue: toggleMobileSidebar,
@@ -36,10 +27,7 @@ const GlobalLayoutContextProvider = ({
 	return (
 		<GlobalLayoutContext.Provider
 			value={{
-				isSidebar,
 				isMobileSidebar,
-				changeSidebar,
-				toggleSidebar,
 				changeMobileSidebar,
 				toggleMobileSidebar,
 			}}
